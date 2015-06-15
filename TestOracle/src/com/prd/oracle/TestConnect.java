69af88b9ada736828b9f923ef9c743045f15ada5
李富 <lifuzz@163.com>
2015-06-15 09:25:46 +0800

    测试odbc

---
+++ TestOracle/src/com/prd/oracle/TestConnect.java
@@ -0,0 +1,45 @@
+package com.prd.oracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 建立与Oracle数据库的连接
+ * @author 半夏微凉
+ *
+ */
+
+public class TestConnect {
+	
+	static String driver;
+	static String url ;
+	static String user;
+	static String password;
+	
+	/**
+	 * 
+	 * 获取数据库连接
+	 * 
+	 * @return 数据库连接Connection
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+		//定义数据库的驱动类
+		driver = "oracle.jdbc.driver.OracleDriver";
+		//拼接访问的url
+		url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		//设置用户名
+		user = "scott";
+		//设置密码
+		password = "lifuz";
+		//加载 驱动
+		Class.forName(driver);
+		
+		//返回数据库连接
+		return DriverManager.getConnection(url, user, password);
+	}
+
+}
---
+++ TestOracle/src/com/prd/oracle/TestMain.java
@@ -0,0 +1,31 @@
+package com.prd.oracle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * 测试数据库连接
+ * @author 半夏微凉
+ *
+ */
+
+public class TestMain {
+
+	public static void main(String[] args) throws Exception {
+		
+		//获取数据库连接
+		Connection conn =  TestConnect.getConnection();
+		
+		String sql = "select * from emp";
+		PreparedStatement pt = conn.prepareStatement(sql);
+		ResultSet rs = pt.executeQuery();
+		
+		while(rs.next()){
+			System.out.println(rs.getString(2));
+		}
+		
+
+	}
+
+}
